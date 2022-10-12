217977e161d12153edd42c72956eefe9454bf85d
Lashareef1 <95951715+Lashareef1@users.noreply.github.com>
2022-10-12 16:42:43 +0530

    Add files via upload

---
+++ BeanExample/src/main/java/com/infinite/bean/DemoBean.java
@@ -0,0 +1,16 @@
+package com.infinite.bean;
+
+public class DemoBean {
+
+	private String greeting = "Good Morining...";
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+	
+	
+}
